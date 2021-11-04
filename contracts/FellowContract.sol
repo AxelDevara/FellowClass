13973f128308cb4805effb981a321f89ee458c3a
Axel Halintar <axeldevara@gmail.com>
2021-11-04 12:31:26 +0700

    added bentobox

--- contracts/FellowContract.sol
+++ contracts/FellowContract.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "hardhat/console.sol";
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 import './interfaces/IERC2981.sol';
 
-contract FellowContract is ERC721, Ownable, IERC2981{
+contract FellowContract is ERC721, Ownable, IERC2981, VRFConsumerBase{
+
+    //const KOVAN_LINK_TOKEN = 0xa36085F69e2889c224210F603D836748e7dC0088;
+    //const KOVAN_VRF_COORDINATOR = 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9;
+    //const KOVAN_KEY_HASH = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
 
     mapping(address => address) fellowAddresses;
     mapping(address => uint) donors;
@@ -28,7 +33,11 @@ contract FellowContract is ERC721, Ownable, IERC2981{
 
     RoyaltyInfo private _royalties;
 
-    constructor() ERC721("FellowAvatar", "FA"){
+    constructor() VRFConsumerBase(_VRFCoordinator, _LinkToken) ERC721("FellowAvatar", "FA"){
+
+    }
+
+    function requestNewRandom(uint userProvidedSeed, string memory name) public {
 
     }
 
@@ -45,7 +54,7 @@ contract FellowContract is ERC721, Ownable, IERC2981{
     /// @param recipient recipient of the royalties
     /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
     function _setRoyalties(address recipient, uint256 value) internal {
-        require(value <= 10000, 'ERC2981Royalties: Too high');
+        require(value <= 20000, 'ERC2981Royalties: Too high');
         _royalties = RoyaltyInfo(recipient, uint24(value));
     }
 
